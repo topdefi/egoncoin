fd095fa0cec8eedd3c86b4d202aca3cde89aacf6
top defi <94203269+topdefi@users.noreply.github.com>
2022-03-21 12:57:50 -0700

    Initial Upload

---
+++ install_Ubuntu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo ""
+echo "Dev-dependencies will now be installed for Ubuntu 18.04"
+echo ""
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install build-essential software-properties-common make curl -y
+
+echo ""
+echo "Installing Go"
+curl -OL https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvf go1.17.6.linux-amd64.tar.gz
+rm -rf go1.17.6.linux-amd64.tar.gz
+
+echo ""
+echo "Build EgonCoin"
+make all
+
+cd build/bin/
+
+./geth init ../../genesis.json
+
+echo ""
+echo "Installation is now complete"
